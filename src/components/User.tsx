bc51e8a85c872eb9cb409213fb9e8bdeb827aa94
Gabryel Boer <107651799+GabryelBoer@users.noreply.github.com>
2023-11-30 19:55:04 -0300

    [feat] Desenvolve componente para perfil do desenvolvedor pesquisado
    - Criado componente para exibir informações do perfil de um desenvolvedor pesquisado, como nome, login, id, foto, localização e seguidores.

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,40 +1,50 @@
 import { UserProps } from "../types/user";
 
 import { MdLocationPin } from "react-icons/md";
+import { IoPerson } from "react-icons/io5";
+import { MdArrowBack } from "react-icons/md";
 
-import { Link } from "react-router-dom";
-
-import classes from "./User.module.css";
+import * as C from "./User.styles";
+import Repo from "./Repo";
 
 const User = ({
   login,
+  id,
+  name,
   avatar_url,
   followers,
-  following,
   location,
+  public_repos,
+  repos,
 }: UserProps) => {
   return (
-    <div className={classes.user}>
-      <img src={avatar_url} alt={login} />
-      <h2>{login}</h2>
-      {location && (
-        <p className={classes.location}>
-          <MdLocationPin />
-          <span>{location}</span>
-        </p>
-      )}
-      <div className={classes.stats}>
-        <div>
-          <p>Seguidores:</p>
-          <p className={classes.number}>{followers}</p>
-        </div>
-        <div>
-          <p>Seguindo:</p>
-          <p className={classes.number}>{following}</p>
-        </div>
-      </div>
-        <Link to={`/repos/${login}`}>Ver melhores projetos</Link>
-    </div>
+    <C.Container>
+      <C.Icon href="/">
+        <MdArrowBack size={"1.7em"} />
+      </C.Icon>
+      <C.Profile>
+        <C.ProfileImg src={avatar_url} alt={login} />
+        <C.Credentials>
+          <C.Name>{name}</C.Name>
+          <C.UserNameID>
+            {login} | {id}
+          </C.UserNameID>
+        </C.Credentials>
+        <C.LastContainer>
+          {location && (
+            <C.LocationContainer>
+              <MdLocationPin />
+              <C.LocationText>{location}</C.LocationText>
+            </C.LocationContainer>
+          )}
+          <C.Stats>
+            <IoPerson />
+            <C.TextNumber>{followers} Seguidores</C.TextNumber>
+          </C.Stats>
+        </C.LastContainer>
+      </C.Profile>
+      <Repo repos={repos} publicRepos={public_repos} />
+    </C.Container>
   );
 };
 
