680fb278a7b24faa8a47b3c5f010d524c51baf4c
Gabryel Boer <107651799+GabryelBoer@users.noreply.github.com>
2023-11-30 19:58:24 -0300

    [feat] Adiciona tipagem do TypeScript para os componentes
    - Adicionada tipagem do TypeScript

---
+++ src/types/search.ts
@@ -0,0 +1,5 @@
+export type TSearch = {
+  items: {
+    login: string;
+  }[];
+};
--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,20 @@
+type Repo = {
+  name: string;
+  id: number;
+  description: string;
+  html_url: string;
+  created_at: string;
+  pushed_at: string;
+  language: string;
+};
+
 export type UserProps = {
   avatar_url: string;
   login: string;
+  name: string;
+  id: number;
   location: string;
   followers: number;
-  following: number;
+  public_repos: number;
+  repos: Repo[];
 };
