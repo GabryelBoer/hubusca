9141d028499fc64c2983d0e61e7cf77c88c0ded7
Gabryel Boer <107651799+GabryelBoer@users.noreply.github.com>
2023-11-30 19:56:53 -0300

    [feat] Implementa componente de pesquisa automática
    - Criado um componente que realiza pesquisas automáticas enquanto o usuário digita, exibindo resultados em tempo real.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,37 +1,98 @@
 type SearchProps = {
   loadUser: (userName: string) => Promise<void>;
+  inputFocus: (inputActive: boolean) => void;
 };
 
-import { useState, KeyboardEvent } from "react";
+import { useState, KeyboardEvent, useEffect, ChangeEvent, useRef } from "react";
 
-import { BsSearch } from "react-icons/bs";
+import { FaSearch } from "react-icons/fa";
 
-import classes from "./Search.module.css";
+import * as C from "./Search.styles";
 
-const Search = ({ loadUser }: SearchProps) => {
+const Search = ({ loadUser, inputFocus }: SearchProps) => {
   const [userName, setUserName] = useState("");
+  const [typingTimeout, setTypingTimeout] = useState<number | null>(null);
+  const [searchButtonClicked, setSearchButtonClicked] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleKeyDown = (e: KeyboardEvent) => {
-    if (e.key == "Enter") {
-      loadUser(userName);
+  const handleTypingFinished = () => {
+    if (!searchButtonClicked && userName.length > 2) {
+      loadUser(inputRef.current?.value || "");
     }
   };
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key == "Enter" && userName.length > 3) {
+      loadUser(inputRef.current?.value || "");
+      setSearchButtonClicked(true);
+    }
+  };
+
+  const handleInputFocus = () => {
+    inputFocus(true);
+  };
+
+  const handleInputBlur = () => {
+    setTimeout(() => {
+      inputFocus(false);
+    }, 100);
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    if (e.target.value === "") {
+      inputFocus(true);
+    } else {
+      inputFocus(false);
+    }
+
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+    setUserName(value);
+    setSearchButtonClicked(false);
+
+    const timeout = setTimeout(() => {
+      handleTypingFinished();
+    }, 1000);
+    setTypingTimeout(timeout);
+  };
+
+  const handleSearchButtonClick = () => {
+    if (userName.length > 3) {
+      setSearchButtonClicked(true);
+      loadUser(inputRef.current?.value || "");
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (typingTimeout) {
+        clearTimeout(typingTimeout);
+      }
+    };
+  }, [userName, typingTimeout, searchButtonClicked]);
+
   return (
-    <div className={classes.search}>
-      <h2>Busque por um usuário:</h2>
-      <p>Conheça seus melhores repositórios</p>
-      <div className={classes.search_container}>
-        <input
-          type="text"
-          placeholder="Digite o nome do usuário"
-          onChange={(e) => setUserName(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        <button onClick={() => loadUser(userName)}>
-          <BsSearch />
-        </button>
-      </div>
-    </div>
+    <>
+      <C.Container>
+        <C.SearchContainer>
+          <C.SearchInput
+            type="text"
+            ref={inputRef}
+            onFocus={handleInputFocus}
+            onBlur={handleInputBlur}
+            placeholder="Digite o nome do usuário"
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          />
+          <C.SearchButton onClick={handleSearchButtonClick}>
+            <FaSearch fill="black" />
+          </C.SearchButton>
+        </C.SearchContainer>
+      </C.Container>
+    </>
   );
 };
 
